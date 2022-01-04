626f637bf697a112cabd1a1247d0cac8a0d7bfd8
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-04 15:47:04 +0100

    Create index.html to check work so far in browser

--- bankTechTest/test/account.test.js
+++ bankTechTest/test/account.test.js
@@ -7,16 +7,4 @@ describe("account", () => {
     expect(account.showBalance()).toBe(0);
   });
 
-  it('can make a deposit', () => {
-    const account = new Account;
-    account.deposit(500)
-    expect(account.showBalance()).toBe(500);
-  });
-
-  it('can withdraw from the balance', () => {
-    const account = new Account;
-    account.deposit(500)
-    account.withdraw(100)
-    expect(account.showBalance()).toBe(400);
-  });
 });
