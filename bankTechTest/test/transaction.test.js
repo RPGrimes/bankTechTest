a328ee9a60f7b76446e297b9d43d46a98398a525
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-04 15:21:45 +0100

    Extraction deposit and withdraw into Transaction classed - also changed how they work

--- bankTechTest/test/transaction.test.js
+++ bankTechTest/test/transaction.test.js
@@ -1,1 +1,15 @@
 const Transaction = require('../src/transaction.js')
+
+describe("transaction", () => {
+
+
+  it('deposits 50 and adds to the balance', () => {
+    const transaction = new Transaction(100, 'deposit', 50)
+    expect(transaction).toEqual({"newBalance": 150, "transactionDate": "04/01/2022", "transactionDoW": "deposit", "transactionValue": 50});
+  });
+
+  it('withdraws 50 and subtracts to the balance', () => {
+    const transaction = new Transaction(100, 'withdraw', 50)
+    expect(transaction).toEqual({"newBalance": 50, "transactionDate": "04/01/2022", "transactionDoW": "withdraw", "transactionValue": 50});
+  });
+});
